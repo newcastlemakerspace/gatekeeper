f8eb4b0226d3924cc990d278e6c46453c7a9197a
Felix Sheldon <darkpaw@internode.on.net>
2014-07-16 15:03:19 +1000

    add original arduino code

---
+++ nfc_reader/cardids.h
@@ -0,0 +1,9 @@
+
+const int NUM_AUTH= 2;
+const int  AUTH_BYTES= 4;
+const  char card1[AUTH_BYTES]=  {0xde, 0xca, 0xfb, 0xad};
+const  char card2[AUTH_BYTES]=  {0xde, 0xad, 0xbe, 0xef};
+  
+const char  * auth_cards[NUM_AUTH] = {(const char*)&card1, (const char *)&card2};  
+  
+
